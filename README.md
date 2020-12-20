78ffd818c65d4a1024ff9c16f86fb378a6f79898
Mark Brooks <Markabrooks@gmail.com>
2020-12-19 23:39:58 -0700

    sends test emails every minute

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # cron-express
+
+`clone repo`
+`npm i`
+`touch .env`
+
+You will need to sign up for a mailtrap account for test emails.
+Sign up at mailtrap.io.  Navigate to your mailbox -> SMTP settings tab -> Integrations
+
+Select nodemailer and add the user and password to your .env file
+
+`.env`
+```
+MAILTRAP_EMAIL=[USER FROM MAILTRAP]
+MAILTRAP_PASSWORD=[PASSWORD FROM MAILTRAP]
+```
+
+to run:
+`node cron`
---
+++ cron.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const cron = require('node-cron')
+require('dotenv').config()
+var nodemailer = require('nodemailer')
+const app = express()
+const port = 3000
+
+//sends every 4 hours
+//cron.schedule("* */4 * * *",()=> {
+
+//sends every minute
+cron.schedule("* * * * *",()=> {
+    console.log("send the email now")
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+        });
+})
+
+var transporter = nodemailer.createTransport({
+host: "smtp.mailtrap.io",
+port: 2525,
+auth: {
+    user: process.env.MAILTRAP_EMAIL,
+    pass: process.env.MAILTRAP_PASSWORD
+}
+});
+
+var mailOptions = {
+from: 'markabrooks@gmail.com',
+to: 'markabrooks@gmail.com',
+subject: 'Sending Email using our sweet node app',
+text: `Hi this is node and we are sending this email`
+};
+
+
+
+// configure routes
+app.get("/", (req, res) => {
+    res.send("index route")
+})
+// start the server
+app.listen(port, () => console.log(`Server: PORT ${port} active`))
